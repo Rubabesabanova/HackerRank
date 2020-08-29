ef95b5c4362eb524952f3f5fa92831b1bb261d49
Ruby <60547132+Rubabe@users.noreply.github.com>
2020-08-29 20:34:43 +0400

    new

---
+++ 30 Days of Code/day_01.py
@@ -0,0 +1,15 @@
+# Difficulty : Easy
+# Link : https://www.hackerrank.com/challenges/30-data-types/problem
+# Language : Python 3
+
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+m = int(input())
+n = float(input())
+p = input()
+
+print(m+i)
+print(d+n)
+print(s+p)
